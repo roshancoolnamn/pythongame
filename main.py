60cf10b89196a99a7b86890b1c75ba5b87bbfd43
roshancoolnamn <roshan.sontineni@hitachigymnasiet.se>
2025-08-24 10:21:04 +0200

    Game has now three parameters, bomb = true?, x position = ? y position = ?

--- main.py
+++ main.py
@@ -2,37 +2,55 @@ import pygame
 
 pygame.init()
 
-class game(pygame.sprite.Sprite):
-   def __init__(self):
-      pygame.sprite.Sprite.__init__(self)
+class Game(pygame.sprite.Sprite):
+   def __init__(self, mine, x, y):
+      if mine == True:
+         pygame.sprite.Sprite.__init__(self)
+         
+         self.image = pygame.Surface([25,25])
+         self.image.fill("red")
+         self.rect = self.image.get_rect()
+         self.rect.x = x
+         self.rect.y = y
+         
+      if mine == False:
+         pygame.sprite.Sprite.__init__(self)
 
-      self.x = 0
-      self.y = 0
-      self.image = pygame.Surface([10,10])
-      self.image.fill("red")
+         self.image = pygame.Surface([25,25])
+         self.image.fill("White")   
+         self.rect = self.image.get_rect()
+         self.rect.x = x
+         self.rect.y = y
+         
 
-      self.rect = self.image.get_rect()
+      
+def CreateGrid():
+   sprite_group.add(Game(False,10,10))
+   sprite_group.add(Game(True,50,10))
+
+   
+     
    
 
-   def update(self):
-      pass
+   
 
 
 screen = pygame.display.set_mode((700,700))
 pygame.display.set_caption("MineSwipper")
 
-block = pygame.sprite.Group()
+sprite_group = pygame.sprite.Group()
 
-block.add(game())
+CreateGrid()
 
 running = True
+screen.fill("Grey") 
 
 while running:
-   block.draw(screen)
+   sprite_group.draw(screen)
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          running = False
-      screen.fill("White")
+      
 
 
    pygame.display.update()
